a5743df0d24f8f5a9bd17db6202a971ddd61de0d
quachvankhanh <khanhqvj@gmail.com>
2025-03-18 17:22:16 +0700

    edit database name

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const dbPath = path.join(__dirname, "../data/database.db");
+const dbPath = path.join(__dirname, "../data/databaseMake.db");
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error connecting to database:", err.message);
