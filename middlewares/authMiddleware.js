8be267a6d9767f834d5767b978404b8cfe8c554b
quachvankhanh <khanhqvj@gmail.com>
2025-03-19 09:24:09 +0700

    add console  log

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 function verifySecretKey(req, res, next) {
+  console.log("have a post data");
   const providedKey = req.headers["x-api-key"];
   if (!providedKey || providedKey !== process.env.SECRET_KEY) {
     return res.status(403).json({ message: "Forbidden: Invalid Secret Key" });
